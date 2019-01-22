93b9971f3434e5cd41645f12bd53e916e3315178
Alfonso Cornejo <alfunkso@outlook.com>
2019-01-22 16:30:39 -0300

    feat/style: added boss pacman victory screen

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {submit} from "../actions";
 import {isSubmittable} from "../selectors";
+import Pacman from './Pacman-1s-200px.svg';
 import '../styles/Submit.css'
 
 const debug = require('debug')('alfunkso.net:Submit');
@@ -15,7 +16,11 @@ function Submit({submittable, submitted, submit}) {
                 submitted
                     ?
                     <div className="SubmitHelperText">
-                        Thank you for your clicking Login!
+                        <div>
+                            Thank you for your clicking Login!
+                            <br />Here's Pacman.
+                        </div>
+                        <img src={Pacman} alt="Pacman" />
                     </div>
                     :
                     <button
